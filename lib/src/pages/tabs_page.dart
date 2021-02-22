16cec149254ad28c29ebbcc70ef547aa2e48277c
Christian Mena <christian.91.gm@hotmail.com>
2021-02-22 16:05:26 -0500

    Implemented navigation

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Tabs Page'),
+    return ChangeNotifierProvider(
+        create: (_) => new _NavigationModel(),
+        child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigation(),
       ),
     );
   }
+}
+
+class _Navigation extends StatelessWidget {
+  const _Navigation({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final navigationModel = Provider.of<_NavigationModel>(context);
+    return BottomNavigationBar(
+      currentIndex: navigationModel.currentPage,
+      onTap: (value) => navigationModel.currentPage = value,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'For You'),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Headers')
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final navigationModel = Provider.of<_NavigationModel>(context);
+    
+    return PageView(
+      controller: navigationModel.pageController,
+      //physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color:Colors.red
+        ),
+        Container(
+          color: Colors.green
+        )
+      ],
+    );
+  }
+}
+
+class _NavigationModel with ChangeNotifier{
+
+  int _currentPage = 0;
+  PageController _pageController = new PageController();
+
+  int get currentPage => this._currentPage;
+
+  set currentPage(int value){
+    this._currentPage = value;
+    _pageController.animateToPage(value, duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => this._pageController;
+  
 }
