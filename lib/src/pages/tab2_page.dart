0b7ec7f7e495bed8db947cf4aa507dfa3c3336c3
Christian Mena <christian.91.gm@hotmail.com>
2021-03-01 15:44:59 -0500

    Implemented list categories

---
+++ lib/src/pages/tab2_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
+import 'package:newsapp/src/services/news_service.dart';
+import 'package:provider/provider.dart';
+
+
+
+class Tab2Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(child: _ListCategories())
+        ],
+      )
+    );
+  }
+}
+
+class _ListCategories extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+  final categories = Provider.of<NewsService>(context).categories;
+
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index){
+        final cName = categories[index].name;
+        return Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              _CategoryButton(category: categories[index]),
+              SizedBox(height: 5),
+              Text('${cName[0].toUpperCase()}${cName.substring(1)}')
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  
+  final Category category;
+
+  const _CategoryButton({this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white
+        ),
+        child: Icon(
+          category.icon,
+          color: Colors.black54
+        ),
+      ),
+    );
+  }
+}
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:newsapp/src/config/config.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 
 class NewsService with ChangeNotifier {
 
   List<Article> headlines = [];
 
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
+
+  
+
   NewsService(){
     this.getTopHeadlines();
   }
 
   getTopHeadlines() async{
-    final url = '$URL_NEWS/top-headlines?apiKey=$NEWS_API_KEY&country=us';
+    final url = '$urlNews/top-headlines?apiKey=$newsApiKey&country=us';
 
     final resp = await http.get(url);
 
